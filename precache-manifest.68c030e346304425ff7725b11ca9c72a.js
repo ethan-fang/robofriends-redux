86b834919e622eb2567cd9ce52142f969dff1262
Ethan Fang <ethanfang@fb.com>
2020-04-06 17:29:24 -0700

    Updates

---
+++ precache-manifest.68c030e346304425ff7725b11ca9c72a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653b13d26569a5dc3b1ec3b80d74247a",
+    "url": "/robofriends-redux/index.html"
+  },
+  {
+    "revision": "1ee3832bec7e69e16aca",
+    "url": "/robofriends-redux/static/css/2.7a3f122f.chunk.css"
+  },
+  {
+    "revision": "09036beafa2d36fcb62f",
+    "url": "/robofriends-redux/static/css/main.43d9c06e.chunk.css"
+  },
+  {
+    "revision": "1ee3832bec7e69e16aca",
+    "url": "/robofriends-redux/static/js/2.570b48e8.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/robofriends-redux/static/js/2.570b48e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09036beafa2d36fcb62f",
+    "url": "/robofriends-redux/static/js/main.1b99fa89.chunk.js"
+  },
+  {
+    "revision": "e140bddcc9ae5da6abeb",
+    "url": "/robofriends-redux/static/js/runtime-main.eab02d27.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
